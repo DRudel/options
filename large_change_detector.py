199c2cea0ccb664e05ca6861ef17d457cab14dcb
David <fwqhgads@gmail.com>
2022-12-26 11:01:57 -0800

    about to test large_change_detector.py

--- large_change_detector.py
+++ large_change_detector.py
@@ -9,6 +9,8 @@ from ffs.train_test import TimeSeriesSplitter, BasicBundleProvider
 from sklearn.base import clone
 from ffs.feature_evaluation import SingleFeatureEvaluationRound
 from datetime import datetime
+from sklearn.preprocessing import StandardScaler
+from ffs.jitter import JitterSetGen
 
 DEFAULT_MODEL_PROTOTYPE = GradientBoostingClassifier(n_estimators=7, random_state=173, max_depth=None)
 
@@ -31,7 +33,7 @@ class LargeChangeDetector:
         self.feature_indexes = feature_indexes
         self.decline = decline
         self.feature_processor = feature_prep
-        self.num_leaves_classification = None
+        self.tuned_leaf_count = None
         self.num_base_leaves = num_base_leaves
         self.additional_feature_leaves = additional_feature_leaves
         self.max_num_features = max_num_features
@@ -39,7 +41,6 @@ class LargeChangeDetector:
         self.transform = None
         self.labels = pd.Series(0, index=self.data)
         self.set_feature_indexes()
-
 
     def set_feature_indexes(self):
         feature_indexes = list(range(len(self.data.columns)))
@@ -61,8 +62,70 @@ class LargeChangeDetector:
         self.data_provider = ComboDataProvider(cont_names=list(data_block.columns), allow_cont_fill=False,
                                                cat_names=[], auxillary_names=[])
 
-    def select_features(self, results_evaluator: ResultEvaluator, possible_indexes=None, established_indexes=None,
-                        min_features=4, **kwargs):
+    def train(self, **kwargs):
+        self.model.max_leaf_nodes = self.tuned_leaf_count
+        training_data = self.create_data_set(set_transform=True, **kwargs)
+        training_features = training_data.iloc[:, : -1]
+        training_labels = training_data.iloc[:, -1]
+        self.model.fit(training_features, training_labels)
+
+    def select_leaf_count(self, model, data_provider, classification, results_evaluator: ResultEvaluator,
+                           features_to_use=None, min_leaves=None, allowed_fails=1, **kwargs):
+        if min_leaves is None:
+            min_leaves = len(self.features_to_use)
+        if min_leaves < 2:
+            min_leaves = 2
+        if features_to_use is None:
+            features_to_use = self.features_to_use
+        bundle_providers = self.get_bundles(data_provider=data_provider, fixed_indexes=features_to_use, **kwargs)
+        best_score = None
+        best_leaf_count = None
+        best_summary = None
+        num_leaves = min_leaves
+        num_fails = -1
+        while num_fails < allowed_fails:
+            my_model = clone(model)
+            my_model.max_leaf_nodes = num_leaves
+            my_round = SingleFeatureEvaluationRound(data_provider=data_provider, model_prototype=my_model,
+                                                    bundle_providers=bundle_providers, max_indexes_better=3,
+                                                    results_evaluator=results_evaluator.score, max_improvement=0.05,
+                                                    established_indexes=features_to_use,
+                                                    use_probability=classification)
+            my_round.compile_data(no_new_features=True)
+            this_score = my_round.summary['score'].iloc[0]
+            if best_score is None or this_score > best_score:
+                best_score = this_score
+                best_leaf_count = num_leaves
+                best_summary = my_round.summary
+                num_fails = -1
+            else:
+                num_fails += 1
+            print(f'num_features = {len(self.features_to_use)}, num_leaves = {num_leaves}; score = {this_score}; '
+                  f'num_fails = {num_fails}', datetime.now())
+            num_leaves += 1
+        self.tuned_leaf_count = best_leaf_count
+
+    def create_data_set(self, set_transform=False, **kwargs):
+        feature_data = self.data.iloc[:, self.feature_indexes].copy()
+        model_feature_data = feature_data.iloc[:, self.features_to_use].copy()
+        t_features = model_feature_data.copy()
+        if set_transform or self.transform is None:
+            scaler = StandardScaler()
+            t_features[:] = scaler.fit_transform(model_feature_data)
+            if set_transform:
+                self.transform = scaler.transform
+        else:
+            t_features[:] = self.transform(model_feature_data)
+        aux_data = pd.DataFrame({
+            'label': self.labels
+        }, index=t_features.index)
+        my_fuzz = JitterSetGen(**kwargs)
+        data_sets = my_fuzz.create_jitter_sets(t_features, dich_features=None, aux_data=aux_data)
+        full_data = pd.concat(data_sets, axis=0)
+        full_data = full_data.dropna()
+        return full_data
+
+    def select_features(self, results_evaluator: ResultEvaluator, established_indexes=None, min_features=4, **kwargs):
         if established_indexes is None:
             established_indexes = []
         end_selection = False
@@ -124,3 +187,11 @@ class LargeChangeDetector:
             my_bundle_provider.generate_trials(random_state=trial_random_state)
             bundle_providers.append(my_bundle_provider)
         return bundle_providers
+
+    def predict(self, data):
+        processed_data = self.feature_processor(data)
+        features = processed_data.iloc[:, self.feature_indexes].copy()
+        probs = self.model.predict_proba(features)
+        features['data'] = data.iloc[:, 0]
+        features['danger'] = probs[:, 1]
+        return features
